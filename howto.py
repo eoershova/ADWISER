b5eab3ea62a3477b3b35349a0877bca45bdf4ab5
eoershova <35444491+eoershova@users.noreply.github.com>
2019-10-25 10:00:52 +0300

    Add files via upload

---
+++ howto.py
@@ -0,0 +1,8 @@
+data = [['sentence word word punctuation mark whatever', ['<sentence NOUN><word VERB><, PUN><whatever EHATEVER>']]]
+
+def adwiser_check(data):
+    #### something happens here###
+
+    processed_data =[['sentence', '0', ''], ['word word', '1', 'check your grammar']]
+
+    return processed_data
