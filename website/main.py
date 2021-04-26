f1df92b60894980654108ba2a73aed36b1685d0b
upunaprosk <irinaproskurina128@gmail.com>
2021-04-26 10:50:26 +0300

    updated_files

--- website/main.py
+++ website/main.py
@@ -48,4 +48,7 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
