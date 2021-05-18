83b97792e1758396aab45105ef587d0a7cef07a3
upunaprosk <72871167+upunaprosk@users.noreply.github.com>
2021-05-18 21:37:03 +0300

    Delete howto.py

--- howto.py
+++
@@ -1,8 +0,0 @@
-data = [['sentence word word punctuation mark whatever', ['<sentence NOUN><word VERB><, PUN><whatever EHATEVER>']]]
-
-def adwiser_check(data):
-    #### something happens here###
-
-    processed_data =[['sentence', '0', ''], ['word word', '1', 'check your grammar']]
-
-    return processed_data
